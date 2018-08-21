bb1a1d9fde8873e13bed1c3975ae6be504a2d477
unknown <sadliergirls@gmail.com>
2018-08-21 15:30:01 -0600

    pushing up for branching

--- src/components/letter.js
+++ src/components/letter.js
@@ -39,36 +39,35 @@ class Letter extends Component {
 
 		render() {
 
-			const inputData = [
-					{title: 'name', state: this.state.name, name: 'name'},
-          {title: 'Good/Bad', state: this.state.goodOrBad, name: 'goodOrBad'},
-          {title: 'Boy/Girl', state: this.state.boyOrGirl, name: 'boyOrGirl'},
-          {title: 'Toy', state: this.state.toyOne, name: 'toyOne'},
-          {title: 'Toy', state: this.state.toyTwo, name: 'toyTwo'},
-          {title: 'Snack', state: this.state.snackOne, name: 'snackOne'},
-          {title: 'Snacc', state: this.state.snackTwo, name: 'snackTwo'},
-          {title: 'Reindeer Snack', state: this.state.reindeerSnack, name: 'reindeerSnack'},
+			   const inputData = [
+					   {title: 'name', state: this.state.name, name: 'name'},
+            {title: 'Good/Bad', state: this.state.goodOrBad, name: 'goodOrBad'},
+            {title: 'Boy/Girl', state: this.state.boyOrGirl, name: 'boyOrGirl'},
+            {title: 'Toy', state: this.state.toyOne, name: 'toyOne'},
+            {title: 'Toy', state: this.state.toyTwo, name: 'toyTwo'},
+            {title: 'Snack', state: this.state.snackOne, name: 'snackOne'},
+            {title: 'Snacc', state: this.state.snackTwo, name: 'snackTwo'},
+            {title: 'Reindeer Snack', state: this.state.reindeerSnack, name: 'reindeerSnack'},
 
-			]
+			     ]
 
-		return (
-			<form onSubmit={this.letterSubmit} className = "letter">
-			<div className = "paragraph">
-
-      if(this.state.hideBoxes) {
-        <p>Dear Santa,
-                 My name is <b>{inputData.name}</b> this year I have been a <b>{data.goodOrBad}</b> <b>{data.boyOrGirl}</b>. For Christmas I would like a/an <b>{data.toyOne}</b>. If you are unable to bring that my backup present would have to be a/an <b>{data.toyTwo}</b>. I will make sure to leave you a snack of <b>{data.snackOne}</b> and <b>{data.snackTwo}</b>. I will also make sure to leave some <b>{data.reindeerSnack}</b> for your reindeer incase they get hungry during your busy night.
-                 Your friend, <b>{data.name}</b>
-        </p>
-      }
-      else {
-        <p>Dear Santa,
-                 My name is {Input(inputData[0], this.handleInputChange)} this year.
-        </p>
-      }
-			</div>
-				<button type ="submit">{!this.state.hideBoxes ? 'Complete' : 'Restart'}</button>
-			</form>
+		         return (
+			            <form onSubmit={this.letterSubmit} className = "letter">
+                  <div>
+                     if(this.state.hideBoxes == true) {
+                       <p>Dear Santa,
+                       My name is <b>{inputData.name}</b> this year I have been a <b>{inputData.goodOrBad}</b> <b>{inputData.boyOrGirl}</b>. For Christmas I would like a/an <b>{inputData.toyOne}</b>. If you are unable to bring that my backup present would have to be a/an <b>{inputData.toyTwo}</b>. I will make sure to leave you a snack of <b>{inputData.snackOne}</b> and <b>{inputData.snackTwo}</b>. I will also make sure to leave some <b>{inputData.reindeerSnack}</b> for your reindeer incase they get hungry during your busy night.
+                        Your friend, <b>{inputData.name}</b>
+                        </p>
+                      }
+                      else {
+                          <p>Dear Santa,
+                          My name is {Input(inputData[0], this.handleInputChange)}.
+                          </p>
+                        }
+			               </div>
+				              <button type ="submit">{!this.state.hideBoxes ? 'Complete' : 'Restart'}</button>
+			              </form>
 
 			)
 	}
